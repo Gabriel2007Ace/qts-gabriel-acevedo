15e6a0d4cd68546f483c54fe79f8f1beb397766f
Gabriel Acevedo <gabriel2007acevedo@gmail.com>
2025-03-12 11:57:38 -0300

    agora esta terminado

--- tests/testeapi_test.js
+++ tests/testeapi_test.js
@@ -1,24 +1,24 @@
 import {
     assertGreater,
-    assertFalse,
-    fail,
-    equal,
-    assertLess
+    assertLess,
+    assertMatch,
 } from "https://deno.land/std/testing/asserts.ts"
-let aluno = 'Cróvis José'
-let professores = ["João", "Lais", "Davi", "Alan"]
 
-
-Deno.test("Teste de assertions (API)", () => {
+Deno.test("Teste maior", () => {
     
     assertGreater(4,2)
     
-    //assertFalse(1!==1)
 
-    //fail(1!==1)
- 
-    //equal(({ foo: "bar" }, { foo: "bar" }))
+});
 
-    assertLess(5, 2)
+Deno.test("Teste menor", () => {
+    
+    assertLess(1, 2)
 
-})
+});
+
+Deno.test("Teste regex", () => {
+    
+    assertMatch("Raptor", /Raptor/)
+
+});
